dd079a9468bc8e9df53d4293894524bcfd5d657e
thanush <104347025+Thanush2412@users.noreply.github.com>
2025-12-13 16:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -972,5 +972,6 @@ def clear_cache():
 
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
